0b00a56b57b70aec2ff8fe6324e10c663067b141
Dean Martin García <dean.martin@gracia.lasalle.cat>
2025-12-02 00:34:42 +0100

    Bloque para recibir el mensaje de la radio y mostrarlo por pantalla.

--- main.ts
+++ main.ts
@@ -2,6 +2,9 @@ input.onButtonPressed(Button.A, function () {
     radio.sendString("Python")
     basic.showString("Enviat")
 })
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
 radio.setGroup(1)
 basic.forever(function () {
 	
